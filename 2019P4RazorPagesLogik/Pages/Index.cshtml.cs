f94d1dc9086f4f9f3b5b71d1329aaffb04e04e3b
Tomáš Kazda <tomas.kazda@pslib.cz>
2019-10-31 06:32:50 +0100

    LogikS InitWindow

---
+++ 2019P4RazorPagesLogik/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Logik.ViewModels;
+
+namespace Logik.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IndexModel()
+        {
+
+        }
+
+        [BindProperty]
+        public LogikInit FromTo { get; set; }
+
+        public void OnGet()
+        {
+            FromTo = new LogikInit() { From = 1, To = 5 };
+        }
+
+        public void OnGetRange(int from, int to)
+        {
+            FromTo = new LogikInit() { From = from, To = to };
+        }
+    }
+}
---
+++ 2019P4RazorPagesLogik/ViewModels/LogikInit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Logik.ViewModels
+{
+    public class LogikInit
+    {
+        [Display(Name = "From number")]
+        [Range(1, 100, ErrorMessage = "Only number from {1} to {2}")]
+        public int From { get; set; }
+
+        [Display(Name = "To number")]
+        [Range(1, 10000, ErrorMessage = "Only number from {1} to {2}")]
+        public int To { get; set; }
+    }
+}
