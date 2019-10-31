a760d67adfc8e3667b5ae9699e838f204d964cfd
Tomáš Kazda <tomas.kazda@pslib.cz>
2019-10-31 20:56:26 +0100

    ViewModel refactoring

--- 2019P4RazorPagesLogik/Pages/Game.cshtml.cs
+++ 2019P4RazorPagesLogik/Pages/Game.cshtml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Logik.Model;
+using Logik.ViewModels;
 
 namespace Logik.Pages
 {
